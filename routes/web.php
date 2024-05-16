1c9ae067f6610b449f9b6c6a3593eebf61374593
daffapratama28 <guling.empapa@gmail.com>
2024-05-16 12:46:56 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/Customer', \App\Http\Controllers\CustomerController::class);
+Route::resource('/Komputer', \App\Http\Controllers\KomputerController::class);
+Route::resource('/Pegawai', \App\Http\Controllers\PegawaiController::class);
